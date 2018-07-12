aa73f6c834f7eaefd68b46e539e2a5ad531bd2e1
Xiaohan Ji <lightenedblade@gmail.com>
2018-07-12 14:26:12 -0700

    Updated Website V.012

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+function modalToggleRun() {
+                var element = document.getElementById("runDivRun");
+                element.classList.toggle("is-active");
+            }
+            function modalToggleLogin() {
+                var element = document.getElementById("runDivLogin");
+                element.classList.toggle("is-active");
+            }
+            function modalToggleReg() {
+                var element = document.getElementById("runDivReg");
+                element.classList.toggle("is-active");
+            }
