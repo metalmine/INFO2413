9a13603f941698f000093fc96d782eaf4bf39436
Xiaohan Ji <lightenedblade@gmail.com>
2018-07-16 13:18:48 -0700

    title comment

--- user/userTemplate.php
+++ user/userTemplate.php
@@ -6,7 +6,9 @@
         <!-- Bulma Set-up -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <title>MHData</title>
+        <!-- JS/PHP Change title to Hunter iD -->
+        <title>Metalmine</title>
+        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
