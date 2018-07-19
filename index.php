51c9f5f91e70529571dfe96f2d414aad7b8a582d
Marcus Nardizzi <puredizzi@gmail.com>
2018-07-19 13:53:40 -0700

    Fixed again

--- index.php
+++ index.php
@@ -7,43 +7,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
 ?>
 <!-- Main container -->
 <section class="hero is-light">
-    <!-- Navigation -->
-    <div class="hero-head">
-        <nav class="navbar">
-            <!-- Logo and Burger-->
-            <div class="navbar-brand">
-                <figure class="image responsive">
-                    <img src="Assets/logo/logo.png">
-                </figure>
-                <!-- Navbar Buttons -->
-                <!-- JS/PHP  Gotta figure out how to do a menu when on a smaller screen-->
-                <div class="navbar-burger" aria-label="menu" aria-expanded="false">
-                    <span id="nav-toggle" class="nav-toggle"></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
-            <!-- Desktop Menu-->
-            <div id="navbarMenuHeroA" class="navbar-menu">
-                <div class="navbar-start">
-                    <!-- Show when Logged out -->
-                    <p class="navbar-item">
-                        <a class="button is-light is-rounded is-dark" onclick="modalToggleLogin()">Login</a>
-                    </p>
-                    <p class="navbar-item">
-                        <a class="button is-light is-rounded is-warning" onclick="modalToggleReg()">Register</a>
-                    </p>
-                    <!-- Show when Logged in -->
-                    <p class="navbar-item">
-                        <a class="button is-dark is-rounded is-hidden" data-target="modal" aria-haspopup="true" onclick="modalToggleRun()">New Run</a>
-                    </p>
-                    <p class="navbar-item">
-                        <a class="button is-dark is-hidden">Account</a>
-                    </p>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/navigation.php");?>
     <!-- Title and Filter -->
     <div class="hero-body">
         <div class="has-text-centered">
