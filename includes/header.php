cedbc4917eb3448a1bbf18763c8ad828cb470913
Marcus Nardizzi <puredizzi@gmail.com>
2018-07-19 22:59:30 -0700

    Reverting the reverted
    Stupid github

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,14 @@
         <!-- Bulma Set-up -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MHData</title>
+        <title>
+            <?php
+            if (!empty($page) && !empty($page['title']))
+                echo $page['title'];
+            else
+                echo "MHData";
+            ?>
+        </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
@@ -15,5 +22,11 @@
         <script src="js/modal.js"></script>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <script src="js/navburger.js"></script>
+
+        <?php
+        if (!empty($page) && !empty($page['scripts']))
+            foreach ($page['scripts'] as $script)
+                echo "<script src=\"$script\"></script>";
+        ?>
     </head>
     <body>
