6965b3445b611a206607f497152290672824a2e6
Marcus Nardizzi <puredizzi@gmail.com>
2018-07-20 23:27:17 -0700

    Some splitting and extra
    Chart also working

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- Google Tracking -->
-        <script src="js/gtag.js"></script>
         <!-- Bulma Set-up -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,19 +12,29 @@
                 echo "MHData";
             ?>
         </title>
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
-        <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+        <script>
+            var jsonData = []
+        </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-29096235-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        <!-- Javascript Imports -->
-        <script src="js/filter.js"></script>
-        <script src="js/modal.js"></script>
+            gtag('config', 'UA-29096235-2');
+        </script>
+        <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js" defer></script>
+        <script src="js/filter.js" defer></script>
+        <script src="js/modal.js" defer></script>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-        <script src="js/navburger.js"></script>
-
+        <script src="js/navburger.js" defer></script>
+        <!-- <script src="js/gen_validatorv4.js" type="text/javascript"></script> -->
         <?php
         if (!empty($page) && !empty($page['scripts']))
             foreach ($page['scripts'] as $script)
-                echo "<script src=\"$script\"></script>";
+                echo "<script src=\"$script\" defer></script>";
         ?>
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,119 +1,22 @@
 <?php
-$page['title'] = '';
-$page['meta'] = '';
+$page['title'] = "MHData";
+$page['meta'] = "";
 $page['scripts'] = [
-    '/Charts/Chart.js'
+    "/Charts/Chart.js",
+    "/js/select.js",
+    "/js/chart-index.js"
 ];
 require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/session.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/db.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
+// require_once('/includes/load.php');
 ?>
 <script>
-    var jsonData = []
-    jsonData['diff'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/monsters-difficulty-list-get.php') ?>;
-    jsonData['type'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/weapons-type-list-get.php') ?>;
-    jsonData['runs_weapons_maps_monsters'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/runs_weapons_maps_monsters-get.php') ?>;
-    // TODO: ^^translation to charts^^
-
-    // TODO: move to seperate file
-    function fillSelect(nValue, nList) {
-		nList.options.length = 1
-		let curr = jsonData['diff'][nValue]
-
-		for (let key in curr) {
-            if (curr.hasOwnProperty(key)) {
-                let nOption = document.createElement('option')
-                nOption.appendChild(document.createTextNode(curr[key].name))
-                nOption.setAttribute("value", curr[key].name)
-                nList.appendChild(nOption)
-			}
-        }
-	}
-
-    // TODO: move to seperate file
-    function typeSelect(nValue, nList) {
-        nList.options.length = 1
-        let curr = jsonData['type'][nValue]
-
-        for (let key in curr) {
-            if (curr.hasOwnProperty(key)) {
-                let nOption = document.createElement('option')
-                nOption.appendChild(document.createTextNode(curr[key].weaponName))
-                nOption.setAttribute("value", curr[key].weaponName)
-                nList.appendChild(nOption)
-            }
-        }
-    }
-
-    // TODO: move to seperate file
-    let ctx = document.getElementById("myChart")
-    let myChart = new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: [
-                "Great Sword",
-                "Sword & Shield",
-                "Dual Blades",
-                "Long Sword" ,
-                "Hammer",
-                "Hunting Horn",
-                "Lance",
-                "Gunlance",
-                "Switch Axe",
-                "Charge Blade" ,
-                "Insect Glavie" ,
-                "Bow" ,
-                "Light Bowgun" ,
-                "Heavy Bowgun",
-                "Event"
-            ],
-            datasets: [{
-                label: 'Amount of Weapons use',
-                data: [
-                    // TODO: Data here -> waiting on data from jsonData['runs_weapons_maps_monsters']
-                ],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                ],
-                pointBorderColor: [
-                    <?php
-                    $count = 14;
-                    $array = [];
-
-                    while ($count-- > 0) {
-                        array_push($array , "'rgba(255,99,132,1)'");
-                    }
-
-                    echo implode(",", $array);
-                    ?>
-                ],
-                pointBackgroundColor: [
-                    <?php
-                    $count = 15;
-                    $array = [];
-
-                    while ($count-- > 0) {
-                        array_push($array , "'rgba(255,99,132,1)'");
-                    }
-
-                    echo implode(",", $array);
-                    ?>
-                ],
-
-            }]
-        },
-        options: {
-            layout: {
-            }
-        }
-    })
+jsonData['diff'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/monsters-difficulty-list-get.php') ?>;
+jsonData['type'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/weapons-type-list-get.php') ?>;
+jsonData['runs_weapons_maps_monsters'] = <?php include($_SERVER['DOCUMENT_ROOT'] . '/sql/runs_weapons_maps_monsters-get.php') ?>;
+// TODO: ^^translation to charts^^
 </script>
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
-// require db queries, do not put query in document
-?>
 <!-- Main container -->
 <section class="hero is-light">
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/components/navigation.php");?>
@@ -131,78 +34,16 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
                 <div class="form-body">
                     <div class="field-body">
                         <!-- Monster Selection -->
-                        <div class="field-label is-normal">
-                            <label class="label is-unselectable">Monster</label>
-                        </div>
-                        <div class="field has-addons">
-                            <p class="control">
-                                <span class="select">
-                                    <select name='states' onchange="fillSelect(this.value, this.form['diff'])">
-            							<option value="">Difficulty</option>
-            							<option value="Low">Low</option>
-            							<option value="High">High</option>
-            							<option value="Tempered">Tempered</option>
-                                    </select>
-                                </span>
-                            </p>
-                            <p class="control">
-                                <span class="select">
-                                    <select name='diff'>
-                                        <option>Select Monster</option>
-                                    </select>
-                                </span>
-                            </p>
-                            <p class="control">
-                                <a class="button is-dark">
-                                    Search
-                                </a>
-                            </p>
-                        </div>
+                        <?php include "./components/monster-selection.php"; ?>
+
                         <!-- Weapon Selection -->
-                        <div class="field-label is-normal">
-                            <label class="label is-unselectable">Weapon</label>
-                        </div>
-                        <div class="field has-addons">
-                            <p class="control">
-                                <span class="select">
-                                    <select name='states' onchange="typeSelect(this.value,this.form['type'])">
-                                        <option value="">Select Type</option>
-                        				<option value="GSD">Great Sword</option>
-                        				<option value="LSD">Long Sword</option>
-                        				<option value="HAM">Hammer</option>
-                        				<option value="HTH">Hunting Horn</option>
-                        				<option value="DBL">Dual Blades</option>
-                        				<option value="SAS">Sword and Sheild</option>
-                        				<option value="LAN">Lance</option>
-                        				<option value="GUL">Gun Lance</option>
-                        				<option value="AWS">Switch Axe</option>
-                        				<option value="CHB">Charge Blade</option>
-                        				<option value="ING">Insect Glaive</option>
-                        				<option value="BOW">Bow</option>
-                        				<option value="LBG">Light Bowgun</option>
-                        				<option value="HBG">Heavy Bowgun</option>
-                        				<option value="EVT">Event Weapons</option>
-                                    </select>
-                                </span>
-                            </p>
-                            <p class="control">
-                                <span class="select">
-                                    <select name='type'>
-                                        <option value="">Select Weapon</option>
-                                    </select>
-                                </span>
-                            </p>
-                            <p class="control">
-                                <!-- Toggles is-dark on click-->
-                                <a class="button" id="filterTASToggle" onclick="tasToggle()">
-                                    Tool Assisted
-                                </a>
-                            </p>
-                        </div>
+                        <?php include "./components/weapon-selection.php"; ?>
+
                         <!-- JS/PHP Hunter Search -->
                         <div class="field-label is-normal">
                             <label class="label is-unselectable">Hunter</label>
                         </div>
+
                         <div class="field has-addons">
                             <div class="control">
                                 <input class="input" type="text" placeholder="Hunter ID / Empty for All">
@@ -269,8 +110,12 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
                                     </table>
                                 </div>
                             </div>
-                            <!-- JS/PHP replace # with a number-->
-                            <footer class="card-footer">
+
+                            <!--
+                                JS/PHP replace # with a number
+                                TODO: split sql
+                            -->
+                            <div class="card-footer">
                                 <a hreff="#" class="card-footer-item">Hunters:
                 				<?php
                 				$count = $pdo->query("SELECT count(*) FROM USERS")->fetchColumn();
@@ -286,14 +131,16 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
                                 // TODO: move to top of page
                 				?>
                 				</a>
-                            </footer>
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <!-- JS/PHP JESSICA Insert Graph Here -->
                 <div class="tile is-6">
 
                 </div>
+
                 <!-- Insert more tables here -->
                 <div class="tile is-4 is-vertical">
                     <!-- JS/PHP replace the data here according to which tab people clicked above -->
@@ -308,66 +155,15 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/header.php");
                         </thead>
                         <tbody>
                             <!-- JS/PHP Replace name with links to weapon/monster to wiki and player to account link -->
-                            <tr>
-                                <th>1</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>2</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>3</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>4</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>5</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>6</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>7</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>8</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>9</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
-                            <tr>
-                                <th>10</th>
-                                <td><a href="https://mhdata.world/Name1">Name 1</a></td>
-                                <td><a href="https://www.youtube.com">00:00</a></td>
-                                <td>12/05/18</td>
-                            </tr>
+                            <?php
+                            for ($i = 1; $i <= 10; $i++)
+                                echo "<tr>
+                                    <th>{$i}</th>
+                                    <td><a href=\"https://mhdata.world/Name1\">Name 1</a></td>
+                                    <td><a href=\"https://www.youtube.com\">00:00</a></td>
+                                    <td>12/05/18</td>
+                                </tr>";
+                            ?>
                         </tbody>
                     </table>
                 </div>
