8aa3098731f1e95d964cdf2c5672d0c49b005c4b
Xiaohan Ji <lightenedblade@gmail.com>
2018-07-30 14:46:43 -0700

    table update

--- includes/table.php
+++ includes/table.php
@@ -1,5 +1,5 @@
 <?php
 $stmt = $pdo->query('SELECT DISTINCT RUNS.runId, USERS.userId, username, time, submitedAt FROM USERS JOIN USERS_RUNS ON USERS.userId = USERS_RUNS.userId JOIN RUNS ON RUNS.runId = USERS_RUNS.runId ORDER BY runId DESC LIMIT 10');
 foreach ($stmt as $row) {
-    echo "<tr> <th> " . $row['runId'] . "</th> <td>", $row['username'] . "</td> <td>", $row['time'] . "</td> <td>", $row['submitedAt'] . "</td> </tr>";
+    echo "<tr> <th> " . $row['runId'] . "</th> <td>", $row['username'] . "</td> <td><a href=\""$row['youtube'] . ">", $row['time'] . "</a></td> <td>", $row['submitedAt'] . "</td> </tr>";
 }
